fe75753f0c3ea88cca3aedea0be734723b120e61
yankewei <yankewei1993@gmail.com>
2026-08-04 23:00:04 +0800

    Initial Commit

---
+++ disco/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  disco
+//
+//  Created by 闫柯玮 on 2026/8/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ disco/discoApp.swift
@@ -0,0 +1,17 @@
+//
+//  discoApp.swift
+//  disco
+//
+//  Created by 闫柯玮 on 2026/8/4.
+//
+
+import SwiftUI
+
+@main
+struct discoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
